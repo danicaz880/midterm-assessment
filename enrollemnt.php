6d12a2e6759fa369682c76dfb3eea144d13528d9
danicaz880 <zdanica439@gmail.com>
2025-09-19 20:05:17 +0800

    Create enrollemnt.php

---
+++ enrollemnt.php
@@ -0,0 +1,24 @@
+student-enrollment/
+│
+├── backend/
+│   ├── db.php
+│   ├── student_api.php
+│   ├── program_api.php
+│   ├── year_api.php
+│   ├── semester_api.php
+│   ├── subject_api.php
+│   └── enrollment_api.php
+│
+├── frontend/
+│   ├── index.html
+│   ├── students.html
+│   ├── programs.html
+│   ├── years_semesters.html
+│   ├── subjects.html
+│   ├── enrollment.html
+│   └── js/
+│       ├── students.js
+│       ├── programs.js
+│       ├── years_semesters.js
+│       ├── subjects.js
+│       └── enrollment.js
